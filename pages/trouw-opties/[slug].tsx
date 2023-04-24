f2cca76401b64fa2b4fcee64e246bbce14be1f94
Bryan de Jong <bryandejong99@gmail.com>
2023-04-24 20:00:43 +0100

    feat: add support for available dates in calendar page

--- pages/trouw-opties/[slug].tsx
+++ pages/trouw-opties/[slug].tsx
@@ -1,3 +1,4 @@
+import { format, lastDayOfMonth, startOfMonth } from "date-fns";
 import { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -32,9 +33,8 @@ import {
 } from "../../src/components";
 import { PageFooterTemplate } from "../../src/components/huwelijksplanner/PageFooterTemplate";
 import { PageHeaderTemplate } from "../../src/components/huwelijksplanner/PageHeaderTemplate";
-import { calendars, CeremonyType } from "../../src/data/huwelijksplanner-state";
-import { Availability } from "../../src/generated";
-import { HuwelijksplannerAPI } from "../../src/openapi";
+import { CalendarEvent } from "../../src/data/huwelijksplanner-state";
+import { AvailabilitycheckService } from "../../src/generated";
 
 export const getServerSideProps = async ({ locale }: { locale: string }) => ({
   props: {
@@ -42,49 +42,24 @@ export const getServerSideProps = async ({ locale }: { locale: string }) => ({
   },
 });
 
-const BlogPost: NextPage = () => {
-  const { locale = "nl", push } = useRouter();
+const PlanningFormPage: NextPage = () => {
+  const { locale = "nl", replace } = useRouter();
   const { t } = useTranslation(["common", "huwelijksplanner-step-2"]);
 
-  const getEvents = (type: CeremonyType, date: string) => {
-    return calendars[type as keyof typeof calendars].filter((event) => event.startDateTime.startsWith(date));
-  };
+  const onCalendarDateSelected = (date: Date) => {
+    console.log(date);
 
-  const [, setResults] = useState<Availability[]>([]);
-
-  const [selectedDate, setSelectedDate] = useState("2021-04-14");
-  const [selectedLocationAndDate, setSelectedLocationAndDate] = useState<string | undefined>();
-
-  const loadEvents = useCallback(() => {
-    const today = new Date().toISOString().replace(/T.+/, "");
-    HuwelijksplannerAPI.getAvailability({
-      interval: "PT1H",
-      start: today,
-    }).then((data) => {
-      setResults(data);
+    AvailabilitycheckService.availabilitycheckGetCollection({
+      interval: "PT2H",
+      start: format(startOfMonth(date.getMonth()), "yyyy-MM-dd"),
+      stop: format(lastDayOfMonth(date.getMonth()), "yyyy-MM-dd"),
+    }).then((result) => {
+      console.log(result);
     });
-  }, []);
-
-  useEffect(() => {
-    loadEvents();
-  }, [selectedDate, loadEvents]);
-
-  const onChangeDateHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setSelectedLocationAndDate(event.target.value);
   };
 
-  const onSelectedDateAndLocationSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // const data = {
-    //   trouw_optie: query.slug,
-    //   locatie_id: selectedLocationAndDate,
-    // };
-
-    if (selectedLocationAndDate) {
-      push("/voorgenomen-huwelijk");
-    } else {
-      throw new Error("please, select a date!");
-    }
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    console.log(event);
   };
 
   return (
@@ -103,7 +78,7 @@ const BlogPost: NextPage = () => {
           <PageContent>
             <BackLink href="/trouw-opties/">← Terug</BackLink>
             <PageContentMain>
-              <form onSubmit={onSelectedDateAndLocationSubmit}>
+              <form onSubmit={onSubmit}>
                 <HeadingGroup>
                   <Heading1>{t("huwelijksplanner-step-2:heading-1")}</Heading1>
                   {/*TODO: Step indicator component */}
@@ -117,55 +92,8 @@ const BlogPost: NextPage = () => {
                 </Paragraph>
                 <section>
                   <FormField>
-                    <Calendar onCalendarClick={(date: string) => setSelectedDate(date)} />
+                    <Calendar onCalendarClick={(date: string) => onCalendarDateSelected(new Date(date))} />
                   </FormField>
-                  <Fieldset>
-                    <FieldsetLegend>Flits/balie-huwelijk — Stadskantoor</FieldsetLegend>
-                    {getEvents("flits-balie-huwelijk", selectedDate).map((event) => (
-                      <FormField key={event.id} type="radio">
-                        <RadioButton2
-                          novalidate={true}
-                          id={event.id}
-                          name="event"
-                          value={event.id}
-                          required
-                          onChange={onChangeDateHandler}
-                        />
-                        <FormLabel htmlFor={event.id} type="radio">
-                          <span aria-label="negen uur tot tien over negen">
-                            <TimeValue dateTime={event.startDateTime} locale={locale} />
-                            {" – "}
-                            <TimeValue dateTime={event.endDateTime} locale={locale} />
-                            {" uur"}
-                          </span>
-                        </FormLabel>
-                      </FormField>
-                    ))}
-                  </Fieldset>
-                  <Fieldset>
-                    <FieldsetLegend>Uitgebreid trouwen — Zelf de plaats bepalen</FieldsetLegend>
-                    {getEvents("uitgebreid-huwelijk", selectedDate).map((event) => (
-                      <FormField key={event.id}>
-                        <RadioButton2
-                          novalidate={true}
-                          id={event.id}
-                          name="event"
-                          value={event.id}
-                          required
-                          onChange={onChangeDateHandler}
-                        />
-                        <FormLabel htmlFor={event.id}>
-                          <span aria-label="negen uur tot tien over negen">
-                            <TimeValue dateTime={event.startDateTime} locale={locale} />
-                            {" – "}
-                            <TimeValue dateTime={event.endDateTime} locale={locale} />
-                            {" uur"}
-                          </span>
-                        </FormLabel>
-                      </FormField>
-                    ))}
-                  </Fieldset>
-
                   <ButtonGroup>
                     <Button type="submit" appearance="primary-action-button">
                       Ja, dit wil ik!
@@ -192,4 +120,4 @@ const BlogPost: NextPage = () => {
   );
 };
 
-export default BlogPost;
+export default PlanningFormPage;
