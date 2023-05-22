32a61fb91eed5ebb190f56a7901acc7dcfb3cc87
Bryan de Jong <bryandejong99@gmail.com>
2023-05-22 15:54:42 +0100

    feat: add form state support to person details page

--- src/components/Checkbox2.tsx
+++ src/components/Checkbox2.tsx
@@ -1,5 +1,5 @@
 import { Checkbox } from "@utrecht/component-library-react";
-import React, { InputHTMLAttributes } from "react";
+import React, { ForwardedRef, InputHTMLAttributes } from "react";
 
 interface Checkbox2Props extends Omit<InputHTMLAttributes<HTMLInputElement>, "type" | "readOnly"> {
   appearance?: string;
@@ -7,10 +7,15 @@ interface Checkbox2Props extends Omit<InputHTMLAttributes<HTMLInputElement>, "ty
   novalidate?: boolean;
 }
 
-export const Checkbox2: React.FC<Checkbox2Props> = ({ novalidate, required, ...props }) => (
-  <Checkbox
-    aria-required={(required && novalidate) || undefined}
-    required={required && !novalidate ? required : undefined}
-    {...props}
-  />
+export const Checkbox2: React.FC<Checkbox2Props> = React.forwardRef(
+  ({ novalidate, required, ...props }, ref: ForwardedRef<HTMLInputElement>) => (
+    <Checkbox
+      aria-required={(required && novalidate) || undefined}
+      required={required && !novalidate ? required : undefined}
+      ref={ref}
+      {...props}
+    />
+  )
 );
+
+Checkbox2.displayName = "Checkbox";
