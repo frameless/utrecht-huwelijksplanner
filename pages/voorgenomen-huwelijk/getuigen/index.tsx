e166afb2198ac7f9a4a06d39fd1db9d1070173e3
Bryan de Jong <bryandejong99@gmail.com>
2023-05-27 17:55:31 +0100

    feat: add skip witness invite button

--- pages/voorgenomen-huwelijk/getuigen/index.tsx
+++ pages/voorgenomen-huwelijk/getuigen/index.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useContext, useId, useState } from "react";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
 import {
   Button,
   ButtonGroup,
@@ -30,6 +30,7 @@ import {
 import { PageFooterTemplate } from "../../../src/components/huwelijksplanner/PageFooterTemplate";
 import { PageHeaderTemplate } from "../../../src/components/huwelijksplanner/PageHeaderTemplate";
 import { MarriageOptionsContext } from "../../../src/context/MarriageOptionsContext";
+import { HuwelijkService } from "../../../src/generated";
 
 export const getServerSideProps = async ({ locale }: { locale: string }) => ({
   props: {
@@ -38,77 +39,55 @@ export const getServerSideProps = async ({ locale }: { locale: string }) => ({
 });
 
 type WitnessType = {
-  name: string;
-  email: string;
+  name?: string;
+  email?: string;
 };
 
 type WitnessFormData = {
-  witnesses: WitnessType[];
+  submit: "invite" | "continue";
+  witnesses?: WitnessType[];
 };
 
 export default function MultistepForm1() {
   const { t } = useTranslation(["common", "huwelijksplanner-step-getuigen", "form"]);
   const [marriageOptions] = useContext(MarriageOptionsContext);
   const { locale = "nl", push } = useRouter();
-  const { register, setError, setFocus, formState, handleSubmit } = useForm<WitnessFormData>();
+  const formMethods = useForm<WitnessFormData>();
   const [loading, setLoading] = useState(false);
 
   const onWitnessSubmit = (formData: WitnessFormData) => {
-    formData.witnesses.forEach((witness, index) => {
-      if (witness.name && !witness.email) {
-        setError(`witnesses.${index}.email`, { type: "required" });
-      }
-      if (!witness.name && witness.email) {
-        setError(`witnesses.${index}.name`, { type: "required" });
-      }
+    console.log(formData);
+    if (formData.submit === "continue") {
+      push("/voorgenomen-huwelijk/getuigen/success");
+      return;
+    }
+
+    if (formData.witnesses) {
+      let hasError = false;
+
+      formData.witnesses.forEach((witness, index) => {
+        if (witness.name && !witness.email) {
+          formMethods.setError(`witnesses.${index}.email`, { type: "required" });
+          hasError = true;
+        }
+        if (!witness.name && witness.email) {
+          formMethods.setError(`witnesses.${index}.name`, { type: "required" });
+          hasError = true;
+        }
+      });
+
+      if (hasError) return;
+    }
+
+    HuwelijkService.huwelijkPatchItem({
+      id: marriageOptions.id as string,
+      requestBody: {
+        getuigen: mapGetuigen(formData),
+      },
+    }).then(() => {
+      push("/voorgenomen-huwelijk/getuigen/succes");
+      setLoading(false);
     });
-  };
-
-  const WitnessFieldset = ({ index }: { index: number }) => {
-    const witnessId = useId();
-    const nameId = useId();
-    const emailId = useId();
-
-    const nameInvalid = !!formState.errors.witnesses?.[index]?.name;
-    const emailInvalid = !!formState.errors.witnesses?.[index]?.email;
-
-    return (
-      <Fieldset>
-        <FieldsetLegend>
-          {t("form:legal-witness")} {index}
-        </FieldsetLegend>
-        <FormField invalid={nameInvalid}>
-          <p className="utrecht-form-field__label">
-            <FormLabel htmlFor={nameId}>{t("form:name")}</FormLabel>
-          </p>
-          {formState.errors.witnesses?.[index]?.name?.type === "required" && (
-            <FormFieldDescription invalid>{t("form:name-required")}</FormFieldDescription>
-          )}
-          <Textbox
-            id={nameId}
-            type="text"
-            autoComplete={`name ${witnessId}`}
-            {...register(`witnesses.${index}.name`)}
-            invalid={nameInvalid}
-          />
-        </FormField>
-        <FormField invalid={emailInvalid}>
-          <p className="utrecht-form-field__label">
-            <FormLabel htmlFor={emailId}>{t("form:email")}</FormLabel>
-          </p>
-          {formState.errors.witnesses?.[index]?.email?.type === "required" && (
-            <FormFieldDescription invalid>{t("form:email-required")}</FormFieldDescription>
-          )}
-          <Textbox
-            id={emailId}
-            type="email"
-            autoComplete={`email ${witnessId}`}
-            {...register(`witnesses.${index}.email`)}
-            invalid={emailInvalid}
-          />
-        </FormField>
-      </Fieldset>
-    );
   };
 
   const formHeaderId = useId();
@@ -139,25 +118,40 @@ export default function MultistepForm1() {
                 <ReservationCard reservation={marriageOptions.reservation} locale={locale} />
               )}
               <section>
-                <form onSubmit={handleSubmit(onWitnessSubmit)} aria-labelledby={formHeaderId}>
-                  <Heading2 id={formHeaderId}>Nodig alvast getuigen uit</Heading2>
-                  <Paragraph>Bij je huwelijk zijn minimaal twee en maximaal vier getuigen nodig.</Paragraph>
-                  <Paragraph>
-                    Het is niet verplicht om hun namen nu al in te vullen. Uiterlijk vier weken voor je huwelijksdatum
-                    moet je de getuigen aanmelden.
-                  </Paragraph>
-                  {/*TODO: Dynamisch tonen hoe lang er nog is om getuigen aan te melden*/}
-                  <WitnessFieldset index={1} />
-                  <WitnessFieldset index={2} />
-                  <WitnessFieldset index={3} />
-                  <WitnessFieldset index={4} />
-                  <ButtonGroup>
-                    <Button appearance="primary-action-button" type="submit">
-                      Verstuur uitnodiging
-                    </Button>
-                    <Button type="submit">Later uitnodigen</Button>
-                  </ButtonGroup>
-                </form>
+                <FormProvider {...formMethods}>
+                  <form onSubmit={formMethods.handleSubmit(onWitnessSubmit)} aria-labelledby={formHeaderId}>
+                    <Heading2 id={formHeaderId}>Nodig alvast getuigen uit</Heading2>
+                    <Paragraph>Bij je huwelijk zijn minimaal twee en maximaal vier getuigen nodig.</Paragraph>
+                    <Paragraph>
+                      Het is niet verplicht om hun namen nu al in te vullen. Uiterlijk vier weken voor je huwelijksdatum
+                      moet je de getuigen aanmelden.
+                    </Paragraph>
+                    {/*TODO: Dynamisch tonen hoe lang er nog is om getuigen aan te melden*/}
+                    <WitnessFieldset index={0} />
+                    <WitnessFieldset index={1} />
+                    <WitnessFieldset index={2} />
+                    <WitnessFieldset index={3} />
+                    <ButtonGroup>
+                      <Button
+                        disabled={loading}
+                        appearance="primary-action-button"
+                        type="submit"
+                        value={"invite"}
+                        {...formMethods.register("submit")}
+                      >
+                        Verstuur uitnodiging
+                      </Button>
+                      <Button
+                        type="submit"
+                        appearance="secondary-action-button"
+                        value={"continue"}
+                        {...formMethods.register("submit")}
+                      >
+                        Later uitnodigen
+                      </Button>
+                    </ButtonGroup>
+                  </form>
+                </FormProvider>
               </section>
             </PageContentMain>
           </PageContent>
@@ -190,4 +184,53 @@ const mapGetuigen = (data: any) => {
   }).filter(Boolean);
 
   return getuigen.filter((g) => g);
+};
+
+const WitnessFieldset = ({ index }: { index: number }) => {
+  const { t } = useTranslation(["form"]);
+  const { register, formState, watch } = useFormContext<WitnessFormData>();
+  const witnessId = useId();
+  const nameId = useId();
+  const emailId = useId();
+
+  const nameInvalid = !!formState.errors.witnesses?.[index]?.name;
+  const emailInvalid = !!formState.errors.witnesses?.[index]?.email;
+
+  return (
+    <Fieldset>
+      <FieldsetLegend>
+        {t("form:legal-witness")} {index}
+      </FieldsetLegend>
+      <FormField invalid={nameInvalid}>
+        <p className="utrecht-form-field__label">
+          <FormLabel htmlFor={nameId}>{t("form:name")}</FormLabel>
+        </p>
+        {formState.errors.witnesses?.[index]?.name?.type === "required" && (
+          <FormFieldDescription invalid>{t("form:name-required")}</FormFieldDescription>
+        )}
+        <Textbox
+          id={nameId}
+          type="text"
+          autoComplete={`name ${witnessId}`}
+          invalid={nameInvalid}
+          {...register(`witnesses.${index}.name`)}
+        />
+      </FormField>
+      <FormField invalid={emailInvalid}>
+        <p className="utrecht-form-field__label">
+          <FormLabel htmlFor={emailId}>{t("form:email")}</FormLabel>
+        </p>
+        {formState.errors.witnesses?.[index]?.email?.type === "required" && (
+          <FormFieldDescription invalid>{t("form:email-required")}</FormFieldDescription>
+        )}
+        <Textbox
+          id={emailId}
+          type="email"
+          autoComplete={`email ${witnessId}`}
+          invalid={emailInvalid}
+          {...register(`witnesses.${index}.email`)}
+        />
+      </FormField>
+    </Fieldset>
+  );
 };
