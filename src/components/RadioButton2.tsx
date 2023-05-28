b4b5e6e9ded1db7e9e8e50f895f902985934db5c
Bryan de Jong <bryandejong99@gmail.com>
2023-05-28 16:12:44 +0100

    feat: add forwardedRef to RadioButton

--- src/components/RadioButton2.tsx
+++ src/components/RadioButton2.tsx
@@ -1,15 +1,21 @@
 import { RadioButton } from "@utrecht/component-library-react";
-import React, { InputHTMLAttributes } from "react";
+import React, { ForwardedRef, InputHTMLAttributes } from "react";
+import { Checkbox2 } from "./Checkbox2";
 
 interface RadioButton2Props extends InputHTMLAttributes<HTMLInputElement> {
   invalid?: boolean;
   novalidate?: boolean;
 }
 
-export const RadioButton2: React.FC<RadioButton2Props> = ({ required, novalidate, ...props }) => (
-  <RadioButton
-    aria-required={(required && novalidate) || undefined}
-    required={required && !novalidate ? required : undefined}
-    {...props}
-  />
+export const RadioButton2: React.FC<RadioButton2Props> = React.forwardRef(
+  ({ required, novalidate, ...props }, ref: ForwardedRef<HTMLInputElement>) => (
+    <RadioButton
+      aria-required={(required && novalidate) || undefined}
+      required={required && !novalidate ? required : undefined}
+      ref={ref}
+      {...props}
+    />
+  )
 );
+
+RadioButton2.displayName = "RadioButton";
