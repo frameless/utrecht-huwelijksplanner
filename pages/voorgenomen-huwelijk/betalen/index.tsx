23ff6b9e4dad1d88e72e538c5a0d2c2c86ba41fd
Robbert Broersma <git@robbertbroersma.nl>
2023-06-07 21:30:38 +0200

    refactor: use `next/link` instead of `passHref`

--- pages/voorgenomen-huwelijk/betalen/index.tsx
+++ pages/voorgenomen-huwelijk/betalen/index.tsx
@@ -4,26 +4,25 @@ import Link from "next/link";
 
 const Betalen: NextPage = () => {
   return (
-    <Link passHref href="/voorgenomen-huwelijk/betalen/succes">
-      <a
+    <Link
+      href="/voorgenomen-huwelijk/betalen/succes"
+      style={{
+        backgroundColor: "#fbfbfb",
+        textAlign: "center",
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+      }}
+    >
+      <Image
+        src="/img/ideal-scherm.png"
+        alt=""
+        width={870}
+        height={1205}
         style={{
-          backgroundColor: "#fbfbfb",
-          textAlign: "center",
-          height: "100%",
-          width: "100%",
-          position: "absolute",
+          boxShadow: "0 0 6px 0 rgba(0,0,0,0.16)",
         }}
-      >
-        <Image
-          src="/img/ideal-scherm.png"
-          alt=""
-          width={870}
-          height={1205}
-          style={{
-            boxShadow: "0 0 6px 0 rgba(0,0,0,0.16)",
-          }}
-        />
-      </a>
+      />
     </Link>
   );
 };
