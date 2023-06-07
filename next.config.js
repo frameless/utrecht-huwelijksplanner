2a56833a6b5c81ccec910b5a06c9b086f4ead0aa
Robbert Broersma <git@robbertbroersma.nl>
2023-06-07 21:31:28 +0200

    build: upgrade to Next 13

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-const withTM = require('next-transpile-modules')(['@utrecht/web-component-library-react']);
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   reactStrictMode: true,
   i18n,
-  ...withTM(),
+  transpilePackages: ['@utrecht/web-component-library-react'],
   async headers() {
     return [
       {
